59d73ee3b6c5aa83595ffa8a83f4e754b0cb6181
LIMON029 <limon00029@gmail.com>
2022-01-17 17:51:07 +0900

    20220117_stop.sh_변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,8 +4,8 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-echo "> $IDEL_PORT 에서 구동 중인 애플리케이션 pid 확인"
-IDLE_PID=${lsof -ti tcp:${IDLE_PORT}}
+echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+IDLE_PID=$(pgrep -fa aws-spring-blog | grep jar | awk '{print $1}')
 
 if [ -z ${IDLE_PID} ]
 then
